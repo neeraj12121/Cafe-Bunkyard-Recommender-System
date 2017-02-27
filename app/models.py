05a32536ee8dbde2b91a63ae9d678bc9ed606f58
Neeraj <ny.neeraj12121@gmail.com>
2017-02-27 23:44:17 +0530

    updated models

--- app/models.py
+++ app/models.py
@@ -6,6 +6,14 @@ from django.db import models
 class menu(models.Model):
     item = models.CharField(max_length=120)
     price = models.IntegerField()
+    rating = models.IntegerField(max_length=10)
+    user_rating = models.IntegerField(max_length=10)
+
+class UserRating(models.Model):
+    name = models.TextField
+    item = models.CharField(max_length=120)
+    person_rating = models.TextField()
+
 
    # def __unicode__(self):
     #    return self.item
