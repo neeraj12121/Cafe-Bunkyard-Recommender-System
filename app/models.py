df55c39a8a5e39c3ba5c5db45b93d2e2167041d0
Neeraj <ny.neeraj12121@gmail.com>
2017-04-05 23:15:54 +0530

    updated models

--- app/models.py
+++ app/models.py
@@ -2,6 +2,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class UserProfile(models.Model):
+    userid = models.IntegerField(default=0)
+    username = models.CharField(max_length=30 , default='')
+    password = models.CharField(max_length=30 , default='')
+
+class Ratings(models.Model):
+    userid = models.IntegerField(default=0)
+    menuid = models.IntegerField(default=0)
+    rating =  models.IntegerField(default=0)
+
 
 class menu(models.Model):
     item = models.CharField(max_length=120)
@@ -13,10 +23,7 @@ class UserRating(models.Model):
     name = models.TextField
     item = models.CharField(max_length=120)
     person_rating = models.TextField()
-
-
-   # def __unicode__(self):
-    #    return self.item
+    
 
     def __str__(self):
         return self.item
