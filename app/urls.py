d8f70d3ea9eaeeeb1d1074d8e1562ec0c5473a46
Neeraj <ny.neeraj12121@gmail.com>
2017-04-08 18:26:32 +0530

    updated

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^login.html$',views.login,name="login"),
+    url(r'^register.html$', views.register,name="register"),
+    url(r'^logout.html$', views.logout, name="logout"),
+
+
+
+]
